9089f8065b309cffc833a0876447ceba02ab6598
Arcgel <darkdusty8000@gmaio.com>
2025-04-23 10:40:48 +0800

    Initialization of Tables, Models, and Controller for Orders
    PREREQUISTE:
    
    None
    
    FEATURES:
    
    Creation of Table / Model Feilds
    
    Product_ID (FK)
    
    Total_price
    
    Initialization of Order Controller
    
    Functions to be added

---
+++ backend/app/Models/Orders.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Orders extends Model
+{
+    protected $table = 'Orders';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+      'product_id',
+      'total_price',
+      'date'
+    ];
+
+    public function products(){
+        return $this->belongsTo(Products::class, 'product_id', 'Itemcode');
+    }
+}
