f32cac3d5c17c0a72e0c2527d7c9e6cc0e6f6660
Christian <chirstiandave120702@gmail.com>
2025-05-02 19:48:34 +0800

    Update the pages

---
+++ frontend/app/Application/Layouts/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import LandingPage from '../Pages/LandingPage/landing'
+
+const HomePage = () => {
+
+  
+
+  return (
+    <div>
+         <LandingPage />
+    </div>
+  )
+}
+
+export default HomePage
