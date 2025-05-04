054cd008c133a36bd00e3c0a66614dfd61618c30
quivus <v.rajime.v@gmail.com>
2025-05-04 19:17:26 +0800

    [ TICKET #14 ]https://trello.com/c/3008beOP/16-ticket-14-create-a-sidebar-for-admin

--- backend/resources/views/Admin/Pages/Sidebar.blade.php
+++ backend/resources/views/Admin/Pages/Sidebar.blade.php
@@ -7,46 +7,40 @@
   <link rel="stylesheet" href="{{asset('css/sidebar.css')}}">
 </head>
 <body>
+
   <div class="sidebar-container">
     <div class="Logo">
-      <img src="assets/components/oop_logo.png" alt="Logo" style="height: 50px; width: 50px;">
+      <img src="/images/oop_logo.png" alt="Logo" style="height: 120px; width: 120px;">
     </div>
 
     <div class="nav-wrapper" style="flex: 1; display: flex; flex-direction: column; justify-content: center;">
       <ul>
-
         <li>
             <a href="{{route('dashboard')}}">
-                <img src="assets/components/dashboard.png" alt="">
+                <img src="/images/dashboard.png" alt="">
             <span class="nav-text">Dashboard</span>
           </a>
         </li>
 
         <li>
             <a href="">
-                <img src="assets/components/sales.png" alt="">
+                <img src="/images/sales.png" alt="">
             <span class="nav-text">Sales</span>
           </a>
         </li>
 
-        <li class="has-submenu">
+        <li>
           <a href="{{route('products')}}">
-            <img src="assets/components/addedproduct.png" alt="">
+            <img src="/images/product.png" alt="">
             <span class="nav-text">Products</span>
           </a>
         </li>
 
-        <li class="has-submenu">
+        <li>
         <a href="">
-        <img src="assets/components/expenses.png" alt="">
-            <span class="nav-text">Add Expenses</span>
+        <img src="/images/expenses.png" alt="">
+            <span class="nav-text">Expenses</span>
           </a>
-          <div class="submenu">
-          <a href="">
-          <img src="assets/components/expenseshistory.png" alt="">
-              <span>Expenses History</span>
-            </a>
-          </div>
         </li>
       </ul>
     </div>
@@ -54,10 +48,17 @@
     <form action="{{route('logout.admin')}}" method="post">
       @csrf
       <button type="submit">
-        <img src="assets/components/exit.png" alt="">
-        <span class="nav-text">Logout</span>
+        <img src="/images/logout.png" alt="">
       </button>
     </form>
   </div>
+
+  <script>
+    function toggleSidebar() {
+      const sidebar = document.getElementById('sidebar');
+      sidebar.classList.toggle('sidebar-hidden');
+    }
+  </script>
+
 </body>
 </html>
